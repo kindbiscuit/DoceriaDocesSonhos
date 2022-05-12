d3199b2a842272f712f3013eb0b0784eda4faba6
Pedro <p077cd@gmail.com>
2022-05-11 21:54:49 -0300

    commit

---
+++ DoceriaDocesSonhos/Manager/Interfaces/IProductManager.cs
@@ -0,0 +1,16 @@
+﻿using DoceriaDocesSonhos.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoceriaDocesSonhos.Manager.Interfaces
+{
+    public interface IProductManager
+    {
+        public IEnumerable<Product> getAllProducts();
+        public Product getProductById();
+
+    }
+}
