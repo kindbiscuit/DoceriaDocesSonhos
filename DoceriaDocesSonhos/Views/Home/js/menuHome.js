bf5536e16e3d011eca44bb8215173c062cdfe749
Pedro <p077cd@gmail.com>
2022-05-26 22:24:09 -0300

    ajustes de organização

---
+++ DoceriaDocesSonhos/Views/Home/js/menuHome.js
@@ -0,0 +1,12 @@
+// menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  function myFunction(menu) {
+    menu.classList.toggle("change");
+  }
