cb1f83a5b7a85930446eb8678f9995478be9cca1
Élida Gouveia <elidagouveia1@gmail.com>
2022-06-30 22:00:49 -0300

    criando eventos e validação

--- DoceriaDocesSonhos/Views/Home/js/newsletterHome.js
+++ DoceriaDocesSonhos/Views/Home/js/newsletterHome.js
@@ -1,4 +1,21 @@
-function enviar() {
-    var inputEnviar = window.document.getElementById('inputEnviar')
-    inputEnviar.innerText = 'Enviado!'
-}
+const inputEnviar = document.getElementById('iptEnviar')
+
+inputEnviar.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    const nome = document.getElementById('nome')
+    const email = document.getElementById('email')
+    const enviar = document.getElementById('inputEnviar')
+
+    if (nome.value == "") {
+        nome.classList.add('erroInput')
+    } else {
+        nome.classList.remove('erroInput')
+    }
+
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 || email.value.indexOf('.') - email.value.indexOf('@') == 1) {
+        email.classList.add('erroInput')
+    } else {
+        email.classList.remove('erroInput')
+    }
+})
