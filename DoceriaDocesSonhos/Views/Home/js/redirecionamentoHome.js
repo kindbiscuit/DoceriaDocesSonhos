7e39b1b7be084e2cacfe4ac19c6381583799fa54
Élida Gouveia <elidagouveia1@gmail.com>
2022-07-13 20:37:09 -0300

    adicionando novo index

---
+++ DoceriaDocesSonhos/Views/Home/js/redirecionamentoHome.js
@@ -0,0 +1,11 @@
+function fazer_pedido() {
+    window.location.replace('pedido.html')
+}
+
+function open_pedidos() {
+    window.location.replace('pedido.html')
+}
+
+function open_carrinho() {
+    window.location.replace('')
+}
